bbd86c9514b087c30280bd5ad3366d7d4f49d59a
sid <sidbharti2005@gmail.com>
2026-09-07 09:46:05 +0530

    add admin requested recruiter

---
+++ src/main/java/com/placement/dto/AdminRecruiterRequest.java
@@ -0,0 +1,35 @@
+package com.placement.dto;
+
+public class AdminRecruiterRequest {
+    private String userName;
+    private String password;
+    private Long companyId;
+
+    public AdminRecruiterRequest() {
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+}
